0230f26a9dadd47b38ad183cbaa81c96daa42a33
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-05-08 11:23:56 -0400

    Added the main Component

--- reactslack/src/App.js
+++ reactslack/src/App.js
@@ -1,18 +1,27 @@
-// import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login/login';
+import Home from './Components/Home/Home'; // Import your Home component
+import { Container } from 'reactstrap';
+import Rightsidebar from './Components/RightSidebar/RightSidebar';
 import Sidebar from './Components/Sidebar/sidebar';
-import './App.css';
-//import Login from './Components/Login/login';
-// import DirectMessage from './Components/SideBar/DirectMessage/directmessage';
-
+import MainComponent from './Components/MainComponent/MainComponent';
+// import './App.css';
 function App() {
   return (
-    <div className="App">
-      {/* <Login/>     */}
-      <Sidebar />
-      <channels />
-      {/* <DirectMessage/> */}
-    </div>
+    <>
+      <Router>
+        <Rightsidebar />
+        <Sidebar />
+        <channels />
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/main" element={<MainComponent />} />
+          </Routes>
+        </Container>
+      </Router>
+    </>
   );
 }
-
 export default App;
