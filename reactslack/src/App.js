3395f6358c0d280670774481a900a940b7b59285
ShailyGH <shailykunal@.com>
2024-05-10 16:43:21 -0400

    fixed jdl file and sidebar

--- reactslack/src/App.js
+++ reactslack/src/App.js
@@ -3,25 +3,23 @@ import Login from './Components/Login/login';
 import Home from './Components/Home/Home'; // Import your Home component
 import { Container } from 'reactstrap';
 import Rightsidebar from './Components/RightSidebar/RightSidebar';
-import Sidebar from './Components/Sidebar/sidebar';
+import Sidebar from './Components/Sidebar/Sidebar';
 import MainComponent from './Components/MainComponent/MainComponent';
 // import './App.css';
 function App() {
   return (
-    <>
-      <Router>
-        <Rightsidebar />
-        <Sidebar />
-        <channels />
-        <Container>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/main" element={<MainComponent />} />
-          </Routes>
-        </Container>
-      </Router>
-    </>
+    <Router>
+      <Rightsidebar />
+      <Sidebar />
+      <channels />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/main" element={<MainComponent />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 export default App;
