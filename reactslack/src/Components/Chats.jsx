888486ac2b5d41f6cfd90fb3d9bacc5944ac09b1
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-05-12 23:25:40 -0400

    made few changes in login

--- reactslack/src/Components/Chats.jsx
+++ reactslack/src/Components/Chats.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import Shaily from "./assets/Shaily_Modi_Photo.jpg";
-import Jordan from "./assets/Jordan.jpg";
+// import React from "react";
+// import Shaily from "./assets/Shaily_Modi_Photo.jpg";
+// import Jordan from "./assets/Jordan.jpg";
 
-const Chats = () => {
-  return (
-    <div className="chats">
-      <div className="userChat">
-        <img src={Shaily} alt="photo" />
-        <div className="userChatInfo">
-          <span>Shaily</span>
-          <p>Hello</p>
-        </div>
-      </div>
+// const Chats = () => {
+//   return (
+//     <div className="chats">
+//       <div className="userChat">
+//         <img src={Shaily} alt="photo" />
+//         <div className="userChatInfo">
+//           <span>Shaily</span>
+//           <p>Hello</p>
+//         </div>
+//       </div>
 
-      <div className="userChat">
-        <img src={Jordan} alt="photo" />
-        <div className="userChatInfo">
-          <span>Jordan</span>
-          <p>What's up</p>
-        </div>
-      </div>
-    </div>
-  );
-};
+//       <div className="userChat">
+//         <img src={Jordan} alt="photo" />
+//         <div className="userChatInfo">
+//           <span>Jordan</span>
+//           <p>What's up</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Chats;
+// export default Chats;
